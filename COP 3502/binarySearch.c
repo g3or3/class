f3691687b6dff3039dffb91912e8a22815da8534
George Vinueza <vinueza.george@gmail.com>
2021-01-28 18:04:11 -0500

    binarySearch file added

---
+++ COP 3502/binarySearch.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void binarySearch(int arr[], int, int);
+
+int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+int main() {
+  binarySearch(arr, 10, 0);
+}
+
+void binarySearch(int arr[], int size, int target) {
+  int lo = 0;
+  int hi = size - 1;
+  int mid;
+
+  while (lo <= hi) {
+    mid = lo + ((hi - lo) / 2);
+    if (arr[mid] == target) {
+      printf("%d is the index\n", mid);
+      return;
+    }
+    if (arr[mid] < target) lo = mid + 1;
+    if (arr[mid] > target) hi = mid - 1;
+  }
+  printf("%d not in the array\n", target);
+}
