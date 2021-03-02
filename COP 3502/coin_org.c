fbedd1352d15daf4e7ea99ae7207d3b650f0c7a9
g3or3 <>
2021-03-01 22:34:20 -0500

    added more

--- COP 3502/coin_org.c
+++ COP 3502/coin_org.c
@@ -21,34 +21,37 @@ customerList * createCustList();
 customer * createCustomer();
 void addCustomer(customerList *, customer *);
 void convertToUnits(customerList *, int, int);
-void sortCustomers();
-void clearMemory();
+void sortCustomers(customerList *);
+void clearMemory(customerList *);
 
 
 int main() {
   int numCustomers, tokens, bills;
-
+  customer * newCustomer;
+  
+  /* create the array list to store new customers */
   customerList * custList = createCustList();
 
   /* number of paying customers */
   scanf("%d", &numCustomers);
 
   while (numCustomers) {
-    customer * person = createCustomer();
-    addCustomer(custList, person);
+    newCustomer = createCustomer();
+    addCustomer(custList, newCustomer);
     numCustomers--;
   }
 
   /* conversion rate a tokens == b bills */
   scanf("%d %d", &tokens, &bills);
   convertToUnits(custList, tokens, bills);
-  // sortCustomers();
-  for (int i = 0; i < custList->size; i++) {
-    printf("%lld\n", custList->customers[i]->units);
-  }
+
+  // sortCustomers(custList);
+  clearMemory(custList);
+
   return 0;
 }
 
+/* Set up an array of customer structs to store every paying customer */
 customerList * createCustList() {
   customerList * custList = calloc(1, sizeof(customerList));
   custList->customers = calloc(1, sizeof(customer*));
@@ -57,25 +60,28 @@ customerList * createCustList() {
   return custList;
 }
 
+/* Create a customer struct to store the name, tokens, bills and converted units */
 customer * createCustomer() {
   char handle[MAX_NAME_LEN];
-  customer * person = calloc(1, sizeof(customer));
-  scanf("%s %d %d", handle, &person->tokens, &person->bills);
-  person->name = calloc(strlen(handle) + 1, sizeof(char));
-  strcpy(person->name, handle);
-  return person;
+  customer * newCustomer = calloc(1, sizeof(customer));
+  scanf("%s %d %d", handle, &newCustomer->tokens, &newCustomer->bills);
+  newCustomer->name = calloc(strlen(handle) + 1, sizeof(char));
+  strcpy(newCustomer->name, handle);
+  return newCustomer;
 }
 
-void addCustomer(customerList * custList, customer * person) {
+/* Add the customer to the array of customers */
+void addCustomer(customerList * custList, customer * newCustomer) {
   if (custList->size == custList->capacity) {
     int newCap = custList->capacity * 2;
     custList->customers = realloc(custList->customers, newCap * sizeof(customer *));
     custList->capacity = newCap;
   }
-  custList->customers[custList->size] = person;
+  custList->customers[custList->size] = newCustomer;
   custList->size++;
 }
 
+/* Use the conversion rate to assign a 'units' value to every customer */
 void convertToUnits(customerList * custList, int tokens, int bills) {
   long long int tokenToUnits, billtoUnits;
   for (int i = 0; i < custList->size; i++) {
@@ -85,6 +91,17 @@ void convertToUnits(customerList * custList, int tokens, int bills) {
   }
 }
 
+/* Sort the list of customers from greatest to least in terms of paid value */
 void sortCustomers() {
 
+}
+
+/* Frees any memory used by the program */
+void clearMemory(customerList * custList) {
+  for (int i = 0; i < custList->size; i++) {
+    free(custList->customers[i]->name);
+    free(custList->customers[i]);
+  }
+  free(custList->customers);
+  free(custList);
 }
